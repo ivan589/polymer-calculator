92d3cf92fb82dbbc3ea29639ef26c0d266b63e7f
Ivan Roth <ivanfr26@gmail.com>
2014-08-03 02:14:31 -0300

    Operations hooked, always summing
    The hooked for numbers and operations is working. Default operation is
    sum. TODO: switch through the available operations.

--- elements/web-calculator/web-calculator.js
+++ elements/web-calculator/web-calculator.js
@@ -1,14 +1,61 @@
 
-
 Polymer({
-	numbers: [7,8,9,4,5,6,1,2,3],
+	numbers: 	[7,8,9,4,5,6,1,2,3],
+	operations: ['=','+','-','x','÷','√','C'],
+
+	number1: 0,
+	number2: 0,
+
+	clear: true,
 
 	theme: 'light',
 	
-	total: 0,
+	monitor		: '0',
+	operation	: '',
 
 	ready: function(){
-		console.log('Calcultor ready');
+		 document.querySelector('web-calculator').addEventListener('btn-tap', this.handleButtonTap);
+	},
+
+	handleButtonTap: function(e){
+		var btn = e.detail.btn;
+
+		if(isNaN(btn)){
+			this.handleOperation(btn);
+		}else{
+			this.handleNumber(btn);
+		}
+	},
+
+	handleNumber: function(n){
+		if(this.clear){
+			this.updateMonitor(n);
+		}else{
+			this.appendMonitor(n);
+		}
+
+		this.number2 = Number(this.monitor);
+	},
+
+	handleOperation: function(o){
+
+		//switch operations
+		this.monitor = this.number1 + this.number2;
+
+
+
+		this.number1 = this.monitor;
+		this.operation = o;
+		this.clear = true;
+	},
+
+	updateMonitor: function(content){
+		this.monitor = content;
+		this.clear = false;
+	},
+
+	appendMonitor: function(content){
+		this.monitor = this.monitor + '' + content;
 	}
 
 });
